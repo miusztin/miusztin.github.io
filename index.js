03d34e0f51df2c9a0e59232d42d027296ebdbf29
Mihai <mihai.iusztin@gmail.com>
2022-07-04 21:00:32 +0300

    prima adaugare pe github

---
+++ index.js
@@ -0,0 +1,22 @@
+console.error('app started');
+
+var myName = 'Tomita';
+var age = 3;
+
+console.info(myName);
+console.info(`My name is ${myName}, and I'm ${age} years old!`);
+age = 2;
+console.info(`My name is ${myName}, and I'm ${age} years old!`);
+age = 4;
+console.info(`My name is ${myName}, and I'm ${age} years old!`);
+
+var jobTitle = '<span>Medic</span> generalist';
+
+var mottoElement = document.getElementById('motto');
+console.log(mottoElement);
+
+mottoElement.innerHTML += ' & ' + jobTitle;
+
+console.log(jobTitle);
+
+console.log(typeof document.getElementById);
