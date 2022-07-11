350019242ec35fa22af0bcf9dc1dca214be53234
Mihai <mihai.iusztin@gmail.com>
2022-07-11 21:44:13 +0300

    switch pages

--- index.js
+++ index.js
@@ -1,22 +1,52 @@
-console.error('app started');
+// console.error('app started');
 
-var myName = 'Tomita';
-var age = 3;
+// var myName = 'Tomita';
+// var age = 3;
 
-console.info(myName);
-console.info(`My name is ${myName}, and I'm ${age} years old!`);
-age = 2;
-console.info(`My name is ${myName}, and I'm ${age} years old!`);
-age = 4;
-console.info(`My name is ${myName}, and I'm ${age} years old!`);
+// console.info(myName);
+// console.info(`My name is ${myName}, and I'm ${age} years old!`);
+// age = 2;
+// console.info(`My name is ${myName}, and I'm ${age} years old!`);
+// age = 4;
+// console.info(`My name is ${myName}, and I'm ${age} years old!`);
 
-// var jobTitle = '<span>Medic</span> generalist';
+// // var jobTitle = '<span>Medic</span> generalist';
 
-var mottoElement = document.getElementById('motto');
-console.log(mottoElement);
+// var mottoElement = document.getElementById('motto');
+// console.log(mottoElement);
 
-mottoElement.innerHTML += ' & ' + jobTitle;
+// mottoElement.innerHTML += ' & ' + jobTitle;
 
-console.log(jobTitle);
+// console.log(jobTitle);
 
-console.log(typeof document.getElementById);
+// console.log(typeof document.getElementById);
+
+function showHomePage() {
+  document.getElementById('home').style.display = 'block';
+  document.getElementById('skills').style.display = 'none';
+  document.getElementById('projects').style.display = 'none';
+  document.getElementById('languages').style.display = 'none';
+}
+
+function showSkillsPage() {
+  document.getElementById('home').style.display = 'none';
+  document.getElementById('skills').style.display = 'block';
+  document.getElementById('projects').style.display = 'none';
+  document.getElementById('languages').style.display = 'none';
+}
+
+function showProjectsPage() {
+  document.getElementById('skills').style.display = 'none';
+  document.getElementById('projects').style.display = 'block';
+  document.getElementById('home').style.display = 'none';
+  document.getElementById('languages').style.display = 'none';
+}
+
+function showLanguagesPage() {
+  document.getElementById('skills').style.display = 'none';
+  document.getElementById('projects').style.display = 'none';
+  document.getElementById('home').style.display = 'none';
+  document.getElementById('languages').style.display = 'block';
+}
+
+showHomePage();
