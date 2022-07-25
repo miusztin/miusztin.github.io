ab5512c644c8eb1f10335c201786027d69c4e21c
Mihai <mihai.iusztin@gmail.com>
2022-07-25 21:24:47 +0300

    addEventListener

--- index.js
+++ index.js
@@ -1,43 +1,31 @@
-// function hide(id) {
-//   console.log('hide', id);
-//   document.getElementById(id).style.display = 'none';
-// }
+function hide(id) {
+  document.getElementById(id).style.display = 'none';
+}
 function show(id) {
-  console.warn('show', id);
   document.getElementById(id).style.display = 'block';
 }
 
 let activePage = 'home';
 function showPage(nextPage) {
+  hide(activePage);
   show(nextPage);
   document.getElementById('menu-' + activePage).classList.remove('active');
-  document.getElementById(activePage).style.display = 'none';
+  // document.getElementById(activePage).style.display = 'none';
   document.getElementById('menu-' + nextPage).classList.add('active');
   activePage = nextPage;
 }
+
+function initEvents() {
+  document
+    .getElementById('top-menu-bar')
+    .addEventListener('click', function (e) {
+      if (e.target.matches('a')) {
+        var id = e.target.id.substring(5);
+        console.warn('click', id, e.target.matches('a'));
+        showPage(id);
+      }
+    });
+}
+
 showPage(activePage);
-
-// const pages = document.querySelectorAll('.page');
-//1-varianta forEach
-// function showPage(id) {
-//   pages.forEach((page) => {
-//     if (page.id === id) {
-//       page.classList = 'page visible';
-//     } else {
-//       page.classList = 'page invisible';
-//     }
-//   });
-// }
-// showPage('home');
-
-//2- varianta for of loop
-// function showPage(id) {
-//   for (let page of pages) {
-//     if (page.id === id) {
-//       page.classList = 'page visible';
-//     } else {
-//       page.classList = 'page invisible';
-//     }
-//   }
-// }
-// showPage('home');
+initEvents();
